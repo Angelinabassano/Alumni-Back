ebe2399f9512a7c38c0f88963c0a18188b0e91cb
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-10-09 21:36:43 +0200

    [AL-138] Feat: AL-140 Endpoint created

--- schools/views.py
+++ schools/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import School
+
+def school_list(request):
+    schools = School.objects.values_list('name', flat=True)
+    return JsonResponse(list(schools), safe=False)
+
