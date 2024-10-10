d3918ff90b05529378a443b130ecb89909d03a7e
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-10-10 10:19:57 +0200

    [AL-138] Feat: Al-140-Updated

--- schools/models.py
+++ schools/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class School(models.Model):
     name = models.CharField(max_length=100)
-
+    objects = models.Manager()
     def __str__(self):
         return self.name
-
