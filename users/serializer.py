b2a0c7f0884251d50aad7804fe8dce2493cc0467
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-10-10 12:09:36 +0200

    AL-18 fixes in user creation

--- users/serializer.py
+++ users/serializer.py
@@ -5,16 +5,33 @@ from .models import User
 class RPSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'school', 'email', 'password']
+        fields = ['first_name', 'last_name', 'email', 'password', 'school', 'role']
+
+    def validate(self, data):
+        if 'rp' not in data['role']:
+            raise serializers.ValidationError('El rol debe ser "rp"')
+        return data
 
 
 class CoderSerializer(serializers.ModelSerializer):
+    rp = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(role='rp'))  # Relacionar con RP
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'school', 'rp', 'linkedin', 'github', 'email', 'password']
+        fields = ['first_name', 'last_name', 'email', 'password', 'rp', 'role']
+
+    def validate(self, data):
+        if 'coder' not in data['role']:
+            raise serializers.ValidationError('El rol debe ser "coder"')
+        return data
 
 
 class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['company_name', 'nif', 'email', 'password', 'phone', 'website']
+        fields = ['company_name', 'nif', 'email', 'password', 'role']
+
+    def validate(self, data):
+        if 'empresa' not in data['role']:
+            raise serializers.ValidationError('El rol debe ser "empresa"')
+        return data
--- users/validators.py
+++ users/validators.py
@@ -1,3 +1,7 @@
+from rest_framework import serializers
+from .models import User
+
+
 class RPValidator:
     def __init__(self, data):
         self.data = data
@@ -7,6 +11,9 @@ class RPValidator:
         for field in required_fields:
             if not self.data.get(field):
                 raise serializers.ValidationError(f"El campo {field} es obligatorio para RP")
+
+        if 'email' in self.data and not self.data['email'].count('@') == 1:
+            raise serializers.ValidationError("El campo 'email' no tiene un formato válido")
 
 
 class CoderValidator:
@@ -19,6 +26,19 @@ class CoderValidator:
             if not self.data.get(field):
                 raise serializers.ValidationError(f"El campo {field} es obligatorio para Coder")
 
+        if 'rp' in self.data and not self.is_valid_rp(self.data['rp']):
+            raise serializers.ValidationError("El RP proporcionado no es válido o no existe")
+
+        if 'email' in self.data and not self.data['email'].count('@') == 1:
+            raise serializers.ValidationError("El campo 'email' no tiene un formato válido")
+
+    def is_valid_rp(self, rp_id):
+        try:
+            rp = User.objects.get(id=rp_id, role='rp')
+            return True
+        except User.DoesNotExist:
+            return False
+
 
 class EmpresaValidator:
     def __init__(self, data):
@@ -29,3 +49,16 @@ class EmpresaValidator:
         for field in required_fields:
             if not self.data.get(field):
                 raise serializers.ValidationError(f"El campo {field} es obligatorio para Empresa")
+
+        if 'nif' in self.data and not self.is_valid_nif(self.data['nif']):
+            raise serializers.ValidationError("El N.I.F proporcionado no tiene un formato válido o ya está en uso")
+
+        if 'email' in self.data and not self.data['email'].count('@') == 1:
+            raise serializers.ValidationError("El campo 'email' no tiene un formato válido")
+
+    def is_valid_nif(self, nif):
+        if len(nif) == 9:
+            if nif[0].isalpha() and nif[1:].isdigit():
+                return True
+        return False
+
